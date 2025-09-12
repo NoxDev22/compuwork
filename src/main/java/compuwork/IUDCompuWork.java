9d9afa06a8be2ed99f4c7caaef1a33cf54c2c82f
G22DevShadow <g22devshadow@gmail.com>
2025-09-12 14:17:50 -0500

    Primera version

---
+++ src/main/java/compuwork/IUDCompuWork.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package compuwork;
+
+/**
+ *
+ * @author Jose Felipe
+ */
+public class IUDCompuWork {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
